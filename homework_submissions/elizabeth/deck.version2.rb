dd5e611b163710fad14349a3f1f617b7f640be3c
EAQUINLAN <EA@EAQUINLAN.COM>
2016-10-12 17:44:18 -0400

    Deleted version2 homework

--- homework_submissions/elizabeth/deck.version2.rb
+++
@@ -1,8 +0,0 @@
-
-
-puts "Hello human. How many players do you have?"
-player_response = gets.chomp.to_i
-(1..player_response).each do |i|
-  puts "Player #{i}'s Hand: '"
-  p deal(shuffled_deck, [], 2)
-end
